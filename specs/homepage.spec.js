c90e37420a97529c618803c352ab3fd2e2ce229f
Felipe Magnon <magnon@textecnologia.com.br>
2021-09-24 09:34:58 -0300

    Inclusão de validação positiva/ negativa

--- specs/homepage.spec.js
+++ specs/homepage.spec.js
@@ -1,10 +1,17 @@
 const { browser } = require("protractor")
 
 describe('Homepage', function() {
-    it('perform a search into the api page', function() {
+    it('Realizar uma pesquisa na página da API com sucesso', function() {
     browser.get('#/api')
     element(by.model('searchTerm')).sendKeys('restart')
     element(by.css('.depth-1')).click()
     expect(element(by.css('.api-title')).getText()).toContain('browser.restart')
     })
+
+    it('Realizar uma pesquisa na página da API sem sucesso', function() {
+        browser.get('#/api')
+        element(by.model('searchTerm')).sendKeys('restart')
+        element(by.css('.depth-1')).click()
+        expect(element(by.css('.api-title')).getText()).not.toContain('browser.restart')
+        })
 })
